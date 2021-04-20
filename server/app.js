c4797556c164a47a83f9ea4e033cf0a797d1ee72
Manuel Campana <manuel.campana@hubbub.net>
2021-04-20 22:09:16 +0100

    added handlebars helpers and variables

--- server/app.js
+++ server/app.js
@@ -1,15 +1,32 @@
-const express = require('express');
-const exphbs  = require('express-handlebars');
-const router = require('./router');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const moment = require("moment");
+const router = require("./router");
 const meta = require("./content/meta.json");
+const article = require("./content/article.json");
+const posts = require("./content/posts.json");
 const app = express();
 const port = 3000;
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-app.set('meta', meta);
+var hbs = exphbs.create({
+  helpers: {
+    formatArticleDate: function (date) {
+      return moment(date).format("dddd DD MMMM YYYY, h:mma");
+    },
+    ifEquals: function (a, b, options) {
+      if (a == b) return options.fn(this);
+      return options.inverse(this);
+    },
+  },
+});
 
-app.use('*/static', express.static('public'));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("meta", meta);
+app.set("article", article);
+app.set("posts", posts);
+
+app.use("*/static", express.static("public"));
 
 app.use(router);
 
